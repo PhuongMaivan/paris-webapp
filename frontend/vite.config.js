2ac1a73a17ffc8f4d7fc07da0dc746be755bd1a6
PhuongMaivan <mvphuong12032004@gmail.com>
2026-05-19 16:54:38 +0700

    Cập nhật cấu hình vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // base: "/paris-webapp/",  // ← tên repo GitHub của bạn
-  base: "/",
+  base: "/paris-webapp/",  // ← tên repo GitHub của bạn
+  // base: "/",
 }); 
