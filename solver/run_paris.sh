cf26c91bd86908d377e6fb0d610dde807f7417f5
PhuongMaivan <mvphuong12032004@gmail.com>
2026-05-19 13:47:37 +0700

    Fix: complete rewrite of solver paths using os.path and lightweight dockerfile

--- solver/run_paris.sh
+++ solver/run_paris.sh
@@ -4,9 +4,8 @@
 SAS_FILE=$1
 PLAN_FILE=$2
 
-# Sửa lại đường dẫn gọi sang thư mục fd nằm cùng cấp với file .sh này
-python3 ./fd/fast-downward.py \
-  --plan-file "$PLAN_FILE" "$SAS_FILE" \
+python3 /mnt/c/paris-webapp/solver/fd/fast-downward.py \
+  --plan-file $PLAN_FILE $SAS_FILE \
   --landmarks lmg="lm_hm(use_orders=False, m=1)" \
   --evaluator "hlm=lmcount(lmg, admissible=True, pref=false)" \
-  --search "eager(single(hlm),reopen_closed=False)"
+  --search "eager(single(hlm),reopen_closed=False)"   
